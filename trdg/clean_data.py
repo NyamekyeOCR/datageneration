0f81f9b94168bbed52d12658e2416ee50c1db3f6
Bismarck Bamfo Odoom <bismarckbamfo91@gmail.com>
2020-11-16 21:33:51 +0000

    Normalizing special characters

--- trdg/clean_data.py
+++ trdg/clean_data.py
@@ -11,17 +11,55 @@ def unicode_to_ascii(s):
 def normalize_twi(s):
     s = unicode_to_ascii(s)
     s = re.sub(r'([!.?])', r' \1', s)
-    s = re.sub(r'[^a-zA-Z.,ƆɔɛƐ!?’\'\"\\\n]+', r'', s)
+    s = re.sub(r'[^a-zA-Z.,ƆɔɛƐ!?’\'\"\\]+', r'', s)
     s = re.sub(r'\s+', r' ', s)
     return s
 
 
 def findall(s):
-    x = re.findall('[ɔↃƆɔↄᴐᵋꜫɛƐꜪ]', s)
-    return x
+    t = []
+    for idx, word in enumerate(s):
+        for x in word:
+            if x in ['Ↄ','Ɔ','ɔ','ↄ','ᴐ','ɛ','Ɛ','ε']:
+                t.append(word)
+    # xx = re.findall(r'([ↃƆɔↄᴐɛƐ])', ' '.join(s)) # Find all matches of special characters
+    # tt = replace(t)
+    # return s + tt
+    return xx
+
+
+def replace(s):
+    for i in range(len(s)):
+        for j in range(len(s[i])):
+            if j != 0:
+                if s[i][j] == 'Ͻ' or s[i][j] == 'ɔ':
+                    t = list(s[i])
+                    t[j] = 'ɔ'
+                    s[i] = ''.join(t)
+                elif s[i][j] == 'ɛ' or s[i][j] == 'ɛ':
+                    t = list(s[i])
+                    t[j] = 'ε'
+                    s[i] = ''.join(t)
+    return s
 
 
 def main():
-    with open('dicts/tw.txt') as f:
+    '''Open text file'''
+    with open('tw.txt') as f:
         data = f.readlines()
-    return data
+    x = findall(data)
+    return x
+
+
+    '''Save cleaned text
+    with open('tw1.txt', 'w') as f:
+        f.write(''.join(x))
+    '''
+
+if __name__ == "__main__":
+    x = main()
+    #t = replace(x)
+    #print(len(t))
+    print(x)
+    #print(x[0:100])
+    # print(t[1])
