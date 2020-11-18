9b3e8525f27505affa1265783ffa6e115f0fe898
Bismarck Bamfo Odoom <bismarckbamfo91@gmail.com>
2020-11-18 23:34:48 +0000

    Full data pipeline

---
+++ trdg/data_pipeline.py
@@ -0,0 +1,12 @@
+import subprocess
+'''
+Data Pipeline
+1. Run clean_data.py
+2. Run run.py
+3. Run rename_files.py
+4. Run generate_gt.py
+5. Run copy_gt.py
+6. Run create_lmdb_dataset.py
+'''
+
+subprocess.run(["ls", "-l"])
