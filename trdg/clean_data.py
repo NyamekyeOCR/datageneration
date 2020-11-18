da4225aa2b6ad7cc51431b77680888a6fb464a5c
Bismarck Bamfo Odoom <bismarckbamfo91@gmail.com>
2020-11-18 10:53:53 +0000

    Fixing special character encoding problem

--- trdg/clean_data.py
+++ trdg/clean_data.py
@@ -2,6 +2,10 @@ import unicodedata
 import re
 
 
+special_char1 = ['Ɔ', 'ɔ', 'ↄ', 'ᴐ', 'ɔ', 'ɔ', 'Ɔ', 'Ↄ', 'Ɔ', 'ɔ', 'ↄ', 'ᴐ', 'Ɔ']
+special_char2 = ['ɛ', 'Ɛ', 'ɛ', 'Ɛ', 'ε', 'ɛ', 'Ɛ', 'ε']
+
+
 '''Covert characters from unicode to ascii'''
 def unicode_to_ascii(s):
     return ''.join(
@@ -11,8 +15,7 @@ def unicode_to_ascii(s):
 
 '''Normalize special characters'''
 def normalize(s):
-    s = unicode_to_ascii(s)
-    # s = re.sub(r'([!.?])', r' \1', s)
+    # s = unicode_to_ascii(s)
     s = re.sub(r'[^a-zA-ZƆɔɛƐↃƆɔↄᴐɛƐε’\'\"\n]+', r'', s)
     s = re.sub(r'\s+', r'', s)
     return s
@@ -20,30 +23,37 @@ def normalize(s):
 
 '''Find all matches of words with special characters and clean them'''
 def findall(s):
+    print('----------------')
+    print('STARTING')
+    print('----------------')
+    print(f'Length of data: {len(s)}')
+    print('Normalizing words')
     s = [normalize(word) for word in s]
     match = []
     for idx, word in enumerate(s):
         for x in word:
-            if x in ['Ↄ','Ɔ','ɔ','ↄ','ᴐ','ɛ','Ɛ','ε']:
+            if x in set(special_char1+special_char2):
                 match.append(s.pop(idx))
-    # xx = re.findall(r'([ↃƆɔↄᴐɛƐ])', ' '.join(s)) # Find all matches of \ special characters
-    print(s)
+    print(f'Length of matched words: {len(match)}')
     clean = replace(match)
+    print('----------------')
+    print('Done')
+    print('----------------')
     return s + clean
 
 
 '''Cleaning'''
 def replace(s):
-    for i in range(len(s)):
-        for j in range(len(s[i])):
-            if j != 0:
-                if s[i][j] == 'Ͻ' or s[i][j] == 'ɔ':
-                    t = list(s[i])
-                    t[j] = 'ɔ'
+    for i, word in enumerate(s):
+        for idx, c in enumerate(word):
+            if idx != 0:
+                if c in set(special_char1):
+                    t = list(word)
+                    t[idx] = 'c'
                     s[i] = ''.join(t)
-                elif s[i][j] == 'ɛ' or s[i][j] == 'ɛ':
-                    t = list(s[i])
-                    t[j] = 'ε'
+                if c in set(special_char2):
+                    t = list(word)
+                    t[idx] = special_char1[-1]
                     s[i] = ''.join(t)
     return s
 
@@ -53,6 +63,9 @@ def main():
     with open('tw.txt') as f:
         data = f.readlines()
     x = findall(data)
+
+    '''Delete blank lines'''
+    x = [line for line in x if line != '']
 
     '''Save cleaned text'''
     with open('tw1.txt', 'w') as f:
