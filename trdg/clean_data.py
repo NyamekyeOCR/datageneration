425fdbb0657edec148e2de7c0ffa687a26a8679c
BismarckBamfo <bismarckbamfo91@gmail.com>
2021-01-21 16:54:57 +0000

    Refactor

--- trdg/clean_data.py
+++ trdg/clean_data.py
@@ -4,10 +4,9 @@ from fire import Fire
 from time import time
 
 
-
 blanks = ['', '\n']
 special_char1_small = [chr(390), chr(596), chr(7440),chr(8580)] #Character codes for small "ɔ"
-specila_char1_big = [chr(1021), chr(8579)]  #Character codes for big "ɔ"
+special_char1_big = [chr(1021), chr(8579)]  #Character codes for big "ɔ"
 special_char2_big = [chr(400)] #Character codes for big "ɛ"
 special_char2_small = [chr(603), chr(949)]  #Character codes for small "ɛ"
 special_chars = special_char1_small + special_char1_big + special_char2_small + special_char2_big
@@ -94,7 +93,7 @@ def main(input, output):
 
     '''Save cleaned text'''
     with open(output, 'w') as f:
-        f.write('\n'.join(x))
+        f.write(''.join(x))
 
     print('----------------')
     print('DONE')
