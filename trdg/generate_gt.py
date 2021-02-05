a34cd5269ee0470b926eb80f96725f1935c0c295
BismarckBamfo <bismarckbamfo91@gmail.com>
2021-02-05 10:04:25 +0000

    Generate gtFile

---
+++ trdg/generate_gt.py
@@ -0,0 +1,27 @@
+
+import os
+import re
+from fire import Fire
+import multiprocessing as Pool
+
+def generate_lmdb(path):
+    if path.endswith('/'):
+        path = path.rstrip('/')
+    p = os.listdir(path)
+    names = []
+    for i, x in enumerate(p):
+        idx = [i.start() for i in re.finditer('_', x)]
+        idx = idx[-1]
+        label = x[:idx]
+        src = f'{path}/{x}'
+        target = f'{path}/word_{i}.jpg'
+        os.rename(src, target)
+        t = f'{target}\t{label}\n'
+        names.append(t)
+
+    with open('gt.txt', 'w') as f:
+        f.writelines(names)
+
+
+if __name__ == '__main__':
+    Fire(generate_lmdb)
