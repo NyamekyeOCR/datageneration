b99c355445d680d5d4a79c2aca8d6affbd9ef274
BismarckBamfo <bismarckbamfo91@gmail.com>
2021-05-13 13:13:21 +0000

    Fixed destination path

--- trdg/copy_files.py
+++ trdg/copy_files.py
@@ -7,7 +7,7 @@ def copy_files(src, dest, num_files):
     p = os.listdir(src)
     for i in range(num_files):
         src = f'{src}/{p[i]}'
-        tgt = f'{src}/{p[i]}'
+        tgt = f'{dest}/{p[i]}'
         shutil.move(src, tgt)
 
 
